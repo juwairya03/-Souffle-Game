dee88a7c8967c578eabe961a15f7df4e2c7c7e38
Juwairya Shaikh <juwairya03@gmail.com>
2018-07-25 14:14:06 -0700

    blahhhhhhhhh

--- Phaser Game/main.js
+++ Phaser Game/main.js
@@ -1,43 +1,74 @@
-var mainState = {  
-    preload: function() { 
-        game.load.spritesheet('player', 'media/orangehatlady.png' , 48 , 48 , 12)
-
-    create: function() { 
+var mainState = {
+   preload: function(){
+ 
+       game.load.spritesheet('chef', 'media/orangehatlady.png',48, 48, 12);
+       game.load.image('back', 'media/backg.png');
         
-        game.stage.backgroundColor = '#3598db';
+       
+   },
+   
+   create: function(){
+       
+       var background = game.add.tileSprite(0, 0, 450,600, 'back'); 
+       background.tileScale.x = 1/2;
+       background.tileScale.y = 1/2;
+       
+       game.physics.startSystem(Phaser.Physics.ARCADE);
+       game.world.enableBody = true;
+       
+       this.chef = game.add.sprite(150,10, 'chef');
+       this.chef.frame = 4; 
+       
+       this.chef.animations.add('walk down', [0,1,2], 10, true);
+       this.chef.animations.add('walk left', [3,4,5], 10, true);
+       this.chef.animations.add('walk right', [6,7,8], 10, true);
+       this.chef.animations.add('walk up', [9,10,11], 10, true);
+       this.chef.animations.add('stop',[1], 10, true);
+       
+       //this.chef.animations.play('walk up');      
+       
+       this.chef.scale.setTo(.7,.7);
+           
+      this.cursor = game.input.keyboard.createCursorKeys();
+      
+   },
 
-        game.physics.startSystem(Phaser.Physics.ARCADE);
-
-        game.world.enableBody = true;
-        
-        this.cursor = game.input.keyboard.createCursorKeys();
-
-        this.player = game.add.sprite(100, 125, 'player');
-        
-    this.player.animations.add('wd', [0,1,2],10, true);
-    this.player.animations.add('wl', [3,4,5],10, true);
-    this.player.animations.add('wr', [6,7,8],10, true);
-    this.player.animations.add('wu', [9,10,11],10, true);
-    
-    this.player.animations.play('wl');
-        
-        },
-
-    update: function() {  
-        this.player.body.gravity.y = 600
-        
-        if (this.cursor.left.isDown){ 
-            this.player.body.velocity.x = -200;
-        }else if(this.cursor.right.isDown) {
-           this.player.body.velocity.x = 200;
-       }else{            this.player.body.velocity.x = 0;
-        } 
-
-     if (this.cursor.up.isDown && this.player.body.touching.down){
-            this.player.body.velocity.y = -250; 
+   update: function(){
+       
+       if(this.cursor.left.isDown){
+          this.chef.body.velocity.x = -75;
+           this.chef.body.velocity.y = 0;
+          this.chef.animations.play('walk left');
+       }
+       
+       else if(this.cursor.right.isDown){
+          this.chef.body.velocity.x = 75;
+           this.chef.body.velocity.y = 0;
+           this.chef.animations.play('walk right');
+      } 
+////         else if {
+////           this.chef.body.velocity.x = 0;
+////          this.chef.animations.play('stop');
+////      }
+//       
+       else if(this.cursor.up.isDown) {
+             this.chef.body.velocity.y = -75;
+           this.chef.body.velocity.x = 0;
+           this.chef.animations.play('walk up');
+       }  
+        else if(this.cursor.down.isDown){
+          this.chef.body.velocity.y = 75;
+            this.chef.body.velocity.x = 0;
+              this.chef.animations.play('walk down');
+        }else{
+             this.chef.body.velocity.x = 0;
+            this.chef.body.velocity.y = 0;
+            this.chef.animations.play('stop');
         }
+       
+       },
+};
 
-
-var game = new Phaser.Game(500, 400);  
-game.state.add('main', mainState);  
+var game = new Phaser.Game(450,600);
+game.state.add('main', mainState);
 game.state.start('main');
